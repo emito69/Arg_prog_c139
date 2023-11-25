29ff310fe7df442b987f074af9223f3d3b6b27d1
gladis <gladisbrites83@gmail.com>
2023-11-25 13:27:00 -0300

    modificando el main

--- src/main/java/ap/app/Sistema.java
+++ src/main/java/ap/app/Sistema.java
@@ -20,6 +20,67 @@ public class Sistema {
        EntityManager em = EntityManagerUtil.getEntityManager();
 
 
+
+
+          /*
+        EntityManager em = EntityManagerUtil.getEntityManager();
+        // System.out.println("EntityManager class ==> " + em.getClass().getCanonicalName());
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        String razon_social = "xxx";
+        String cuit = "xxx";
+        Cliente cliente = new Cliente();
+        Timestamp fecha = Timestamp.from(Instant.now());
+
+        cliente.setCuit("xxx");
+        cliente.setRazon_social("xxx");
+        cliente.setNombre("xxx");
+        cliente.setApellido("xxx");
+        cliente.setFechaNacim(fecha);
+
+        em.persist(cliente);
+
+        InfoContacto infoContacto = new InfoContacto();
+        infoContacto.setCelular("1111");
+        infoContacto.setEmail("xxx");
+        infoContacto.setDireccion("xxx");
+        infoContacto.setTelefono("xxx");
+
+        em.persist(infoContacto);
+
+        cliente.setInfoContacto(infoContacto);
+        em.merge(cliente);
+
+        tx.commit();
+        System.out.println("Objeto guardado!!!");
+        */
+
+    /*    EntityManager em = EntityManagerUtil.getEntityManager();
+
+        ClienteController cc = new ClienteController();
+        //seteando el entityManager al repository
+        cc.getCr().setEm(em);
+
+        Scanner scanner = new Scanner(System.in);
+
+        cc.agregarCliente(scanner);
+
+        cc.mostrarClientes();
+
+        //Cliente cliente1 = cc.buscarClienteId(1);
+
+        //cliente1.setNombre("Nadia");
+
+        //cc.actualizarCliente(cliente1);
+
+        cc.mostrarClientes();*/
+
+
+
+
+
         AplicacionController ac = new AplicacionController();
         ac.getAr().setEm(em);
 
